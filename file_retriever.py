e04bb8c32c7606307888dff77947e4be58eb8b1e
MuhammadBilalKhan267 <130090003+MuhammadBilalKhan267@users.noreply.github.com>
2023-12-02 19:54:05 +0500

    Forward Index

--- file_retriever.py
+++ file_retriever.py
@@ -1,6 +1,6 @@
 
 import os
-
+import json
 def get_json_file_paths(root_dir):
     json_file_paths = []
     for root, dirs, files in os.walk(root_dir):
@@ -8,3 +8,10 @@ def get_json_file_paths(root_dir):
             if file.endswith('.json'):
                 json_file_paths.append(os.path.join(root, file))
     return json_file_paths
+
+def load_file(filename):
+    if os.path.exists(filename) and os.path.getsize(filename) > 0:
+        with open(filename, 'r') as urls:
+            return json.load(urls)
+    else:
+        return {}
